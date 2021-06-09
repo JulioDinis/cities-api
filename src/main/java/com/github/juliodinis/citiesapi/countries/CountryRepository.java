4c831562ad9b6a1116bb3dc74b2699e821e19f0c
Julio Dinis <lima.juliolima.julio45@gmail.com>
2021-06-09 09:55:52 -0300

    Primeiro commit

---
+++ src/main/java/com/github/juliodinis/citiesapi/countries/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.github.juliodinis.citiesapi.countries;
+
+import com.github.juliodinis.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+
+}
